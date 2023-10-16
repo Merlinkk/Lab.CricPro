5acfb00530b6bccce10524885c1f16ec8a68e694
Merlinkk <anshulkashyap2504@gmail.com>
2023-10-16 12:38:14 +0530

    Final page done

---
+++ script.js
@@ -0,0 +1,46 @@
+const  virender = document.getElementById('VS')
+const sachin = document.getElementById('ST')
+const gautam = document.getElementById('GG')
+const virat = document.getElementById('VK')
+const ms = document.getElementById('MS')
+const yuvraj = document.getElementById('YS')
+const suresh = document.getElementById('SR')
+const harbhajan = document.getElementById('HS')
+const zaheer = document.getElementById('ZR')
+const munaf = document.getElementById('MP')
+const sreesanth = document.getElementById('SSR')
+
+const infoDiv = document.getElementById('info')
+
+const players = ['https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-sehwag.png',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-sachin.png',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-gambhir.png',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-virat-kohli.png',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-dhoni.png',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-yuvraj.png',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-suresh-raina.jpg',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-harbhajan.jpg',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-zaheer-khan.jpg',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-munaf-patel.jpg',
+                'https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/front-end-web-development/cricpro-sreeshanth.jpg']
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+      onEscapeKeyPressed();
+    }
+  });
+
+
+
+function pointMe(index){
+    console.log(index)
+    infoDiv.style.display = ''
+    infoDiv.style.backgroundImage = `url('${players[index]}')`;
+}
+
+function onEscapeKeyPressed(){
+    infoDiv.style.display = 'none'
+}
+
+
+
